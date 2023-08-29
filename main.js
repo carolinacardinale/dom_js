18674c193368c687c85916ae72871635ab22c3d2
Carolina Leite Cardinale <96205213+carolinacardinale@users.noreply.github.com>
2023-08-29 11:38:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function calcular(){
+    var numero1 = Number(document.getElementById("num1").value)
+    var numero2 = Number(document.getElementById("num2").value) 
+
+    if(numero1>numero2){
+        alert("O número 1 é o maior") 
+    }
+    else
+    {
+        alert ("O número 2 é o maior")
+    }
+}
